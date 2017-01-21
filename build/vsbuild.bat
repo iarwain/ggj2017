1b260c37d947cef06bb0851a7a4f41424b4388bf
iarwain <iarwain@orx-project.org>
2017-01-21 11:25:06 -0500

    - Windows automated build

--- build/vsbuild.bat
+++ build/vsbuild.bat
@@ -1,6 +1,15 @@
+# Inits environment
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 
+# Generates solution
 premake4 vs2015
 
+# Builds Debug & Release
 msbuild windows\vs2015\Waves.sln /p:Configuration=Debug
 msbuild windows\vs2015\Waves.sln /p:Configuration=Release
+
+# Commits changes
+hg commit -m "- Windows automated build"
+
+# Pushes them
+hg push
